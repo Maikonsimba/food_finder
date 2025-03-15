7d8899fa4da386ff2b1de4a0f1d8d84e42a6803a
maiko mhali <maikomhali@gmail.com>
2025-03-16 04:06:14 +1400

    Added STATIC_ROOT to settings

--- food_finder/settings.py
+++ food_finder/settings.py
@@ -136,3 +136,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import os
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
